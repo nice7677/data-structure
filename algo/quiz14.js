c198185136e9be7993dbdb69a8cd249b2104d3ac
진우 <nice7677@naver.com>
2023-01-04 08:26:20 +0900

    q14

--- algo/quiz14.js
+++ algo/quiz14.js
@@ -1,3 +1,12 @@
 function quiz14(x) {
-    return
+    if (x <= 2) return 1;
+    return quiz14(x - 1) + quiz14(x - 2);
 }
+
+console.log(quiz14(4))
+
+// 1, 1, 2, 3, 5, 8, ...
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
