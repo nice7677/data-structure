425cf5d165d8357bbac59fe884d1b91e27217b24
진우 <nice7677@naver.com>
2023-01-04 16:44:31 +0900

    자바도 추가

---
+++ algo/java/algo/src/main/java/kr/springboot/algo/quiz/Quiz15.java
@@ -0,0 +1,20 @@
+package kr.springboot.algo.quiz;
+
+public class Quiz15 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static String[] reverse(String str) {
+
+    }
+
+}
+
+/**
+ * // reverse('awesome') // 'emosewa'
+ * // reverse('rithmschool') // 'loohcsmhtir'
+ */
